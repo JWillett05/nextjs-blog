fe3a08199835064e4beb2fbc40458faf383ff74b
JWillett05 <jack.yuze@uk>
2024-09-18 18:01:55 +0100

    Cart background

--- src/components/CartHeader.jsx
+++ src/components/CartHeader.jsx
@@ -2,12 +2,12 @@ import styles from "@/page.module.css"
 
 export default function CartHeader() {
     return (
-        <>
-        <h1 className="cartHeader">Cart</h1>
-        {/* <p>{items}{priceOfItems}</p> */}
-        {/* <button className={style.orderMoreButton} onClick={handleClick}>Order More</button> */}
-        {/* <p>Total {totalOfItems}</p> */}
-        <button className={styles.checkoutButton}>Checkout {/*{totalOfItems}*/} </button>
-        </>
+        <div className={styles.cartBackground}>
+            <h1 className={styles.cartHeader}>Cart</h1>
+            {/* <p>{items}{priceOfItems}</p> */}
+            {/* <button className={style.orderMoreButton} onClick={handleClick}>Order More</button> */}
+            {/* <p>Total {totalOfItems}</p> */}
+            <button className={styles.checkoutButton}>Checkout {/*{totalOfItems}*/} </button>
+        </div>
     );
   }
