d53345fa319878fabf3c35e5b01ca1b00790f9d0
JWillett05 <jack.yuze@uk>
2024-09-18 14:48:37 +0100

    added all id numbers

--- layout.js
+++ layout.js
@@ -25,7 +25,25 @@ export const CartContext = createContext([])
 export default function layout(){
   const cartItems = [
     {id: 1},
-    {id: 2}
+    {id: 2},
+    {id: 3},
+    {id: 4},
+    {id: 5},
+    {id: 6},
+    {id: 7},
+    {id: 8},
+    {id: 9},
+    {id: 10},
+    {id: 11},
+    {id: 12},
+    {id: 13},
+    {id: 14},
+    {id: 15},
+    {id: 16},
+    {id: 17},
+    {id: 18},
+    {id: 19},
+    {id: 20}
   ]
   return(
     <main className="layout">
